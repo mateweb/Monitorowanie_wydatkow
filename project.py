49fe49d0fb1a072c0429bcf9b9dbb2bf02d73414
mateweb <matewebstudio@gmail.com>
2022-10-09 01:26:22 +0200

    Definiowanie + pętla

---
+++ project.py
@@ -0,0 +1,9 @@
+
+def show_menu ():
+    option1 = print("1. Przegladaj biezace koszta.")
+    option2 = print("2. Dodaj nowy wydatek. ")
+
+
+while True:
+    show_menu ()
+    print(int(input("Wybierz pozycje [1-5] i nacisnij Enter: ")))
