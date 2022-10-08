166c1189084bf33f10c67fc38ac5785d648bbf38
mateweb <matewebstudio@gmail.com>
2022-10-09 01:40:30 +0200

    Dodanie petli

--- project.py
+++ project.py
@@ -1,9 +1,13 @@
 
-def show_menu ():
-    option1 = print("1. Przegladaj biezace koszta.")
-    option2 = print("2. Dodaj nowy wydatek. ")
-
-
 while True:
-    show_menu ()
-    print(int(input("Wybierz pozycje [1-5] i nacisnij Enter: ")))
+    print()
+    month = int(input("Wybierz miesiac [1-12]: "))
+
+    if month == 0:
+        break
+
+    while True :
+        print()
+        print("1. Wybierz miesiac ")
+        print("2. Przegladaj biezace wydatki ")
+        print("3. Dodaj nowy wydatek ")
