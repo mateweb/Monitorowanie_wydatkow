1e117346d5f8e9c23f2055219b9d7bf01389a847
mateweb <matewebstudio@gmail.com>
2022-10-11 01:31:08 +0200

    Dwie podstawowe pętle

--- project.py
+++ project.py
@@ -1,7 +1,4 @@
-import sys
-import os
-
-# while - user should to choice one Month
+# Main while
 while True:
     print()
     month = int(input("Wybierz miesiąc [1-12]: "))
@@ -9,7 +6,31 @@ while True:
     if month == 0:
         break
 
+# Secondary while
     while True:
         print()
         print("0. Powrót do wyboru miesiąca")
         print("1. Przeglądaj bieżące wydatki")
+        print("2. Dodaj wydatek")
+        print("3. Usuń wydatek")
+        print("4. Statystyki")
+        print("5. Zamknij program")
+        print()
+        user_choice = int(input("Wybierz pozycję z menu [0-5] i naciśnij Enter: "))
+        print()
+
+        if user_choice == 0:
+            break
+        if user_choice == 1:
+            print("Przegląd aktualnych kosztów")
+            print("------------------------------")
+        if user_choice == 2:
+            print("Dodaj nowy wydatek")
+            print("------------------------------")
+        if user_choice == 3:
+            print("Usuń istniejący wydatek")
+            print("------------------------------")
+        if user_choice == 4:
+            print("Statystyki")
+            print("------------------------------")
+       
